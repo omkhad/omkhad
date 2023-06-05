ab6d29125be1b69caebafaf7bb6a5694f26e6998
omkhad <omkarkhade76@gmail.com>
2023-06-05 23:25:52 +0530

    adding new project

---
+++ LibraryManagementSystem/src/main/java/com/mvcjsp/connection/DbDetails.java
@@ -0,0 +1,9 @@
+package com.mvcjsp.connection;
+
+public interface DbDetails {
+
+	String DBDRIVER = "com.mysql.cj.jdbc.Driver";
+	String CONSTR = "jdbc:mysql://localhost:3306/userdata?useSSL=false";
+	String USERNAME = "root";
+	String PASSWORD = "Sarvesh@9892";
+}
